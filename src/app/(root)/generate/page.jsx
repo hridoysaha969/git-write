19d191a4ddba500eb3f052bdfe6ddffb25205919
Hridoy Saha <hridoysaha969@gmail.com>
2025-02-07 13:20:49 +0600

    Seo update

--- src/app/(root)/generate/page.jsx
+++ src/app/(root)/generate/page.jsx
@@ -20,3 +20,60 @@ export default function Page() {
     </SectionProvider>
   );
 }
+
+export async function generateMetadata() {
+  return {
+    title: "README Generator - GitWrite",
+    description:
+      "Generate professional README files for your GitHub projects with GitWrite. Input your project details, preview your README, and download it instantly.",
+    keywords: [
+      "README Generator",
+      "README maker",
+      "Professional README Tool",
+      "Effortless README Creator",
+      "GitHub README Maker",
+      "Generate README Files Instantly",
+      "Developer README Tool",
+      "Automated README Generator",
+      "Time-Saving README Generator",
+      "README Generator for Developers",
+      "README Generator for GitHub",
+      "GitWrite",
+      "GitWrite README Generator",
+      "GitWrite README Maker",
+      "Professional README Generatot",
+      "Professional README Maker",
+      "GitHub README Generator",
+    ],
+    openGraph: {
+      title: "README Generator - GitWrite",
+      description:
+        "Generate professional README files for your GitHub projects with GitWrite. Input your project details, preview your README, and download it instantly.",
+      url: "https://gitwrite.vercel.app",
+      type: "website",
+      images: [
+        {
+          url: "/gitwrite_logo.png", // Replace with your image URL
+          width: 1200,
+          height: 1200,
+          alt: "README Generator - GitWrite",
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: "README Generator - GitWrite",
+      description:
+        "Generate professional README files for your GitHub projects with GitWrite. Input your project details, preview your README, and download it instantly.",
+      images: ["/gitwrite_logo.png"], // Replace with your image URL
+    },
+    robots: {
+      index: true,
+      follow: true,
+      googleBot: {
+        index: true,
+        follow: true,
+      },
+    },
+  };
+}
