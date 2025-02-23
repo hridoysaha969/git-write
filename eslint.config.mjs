326b769d2b0defd353962befbc6380c547e5c96d
Hridoy Saha <hridoysaha969@gmail.com>
2025-02-23 21:49:58 +0600

    eslint update

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,17 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 
+// Set up directory paths
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+// Create an instance of FlatCompat
 const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// Make sure compat.extends returns an array of strings and not functions
+const eslintConfig = compat.extends("next/core-web-vitals");
 
 export default eslintConfig;
