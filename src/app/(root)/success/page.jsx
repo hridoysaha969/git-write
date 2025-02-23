44d557c12f83f8cd991b7f58c87b91060093c932
Hridoy Saha <hridoysaha969@gmail.com>
2025-02-23 21:44:18 +0600

    Minor Update

--- src/app/(root)/success/page.jsx
+++ src/app/(root)/success/page.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
 import { notFound, useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 
 export default function PaymentSuccess() {
   const searchParams = useSearchParams();
@@ -37,48 +37,50 @@ export default function PaymentSuccess() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      {loading ? (
-        <div className="py-3 px-6 rounded-md bg-white shadow-md">
-          <p className="flex items-center gap-2">
-            Verifying Payment <Loader2 className="w-5 h-5 animate-spin" />
-          </p>
-        </div>
-      ) : (
-        <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md text-center">
-          <motion.div
-            initial={{ scale: 0 }}
-            animate={{ scale: 1 }}
-            transition={{ duration: 0.5, type: "spring" }}
-            className="flex justify-center"
-          >
-            {success ? (
-              <CheckCircle className="text-green-500 w-20 h-20" />
-            ) : (
-              <OctagonX className="text-red-500 w-20 h-20" />
-            )}
-          </motion.div>
-          <h1 className="text-2xl font-bold mt-4 text-gray-800">
-            {success ? "Payment Successful!" : "Payment Failed!"}
-          </h1>
-          <p className="text-gray-600 mt-2">
-            {success
-              ? `Thank you for your purchase. Your transaction has been completed
-            successfully.`
-              : "Cannot complete your payment! Please try again."}
-          </p>
-
-          <div className="mt-6 space-y-4">
-            <Button asChild>
-              <Link href="/">
-                <button className="w-full py-2 px-4 rounded-lg transition">
-                  Go to Home
-                </button>
-              </Link>
-            </Button>
+    <Suspense fallback={<p>Loading...</p>}>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        {loading ? (
+          <div className="py-3 px-6 rounded-md bg-white shadow-md">
+            <p className="flex items-center gap-2">
+              Verifying Payment <Loader2 className="w-5 h-5 animate-spin" />
+            </p>
           </div>
-        </div>
-      )}
-    </div>
+        ) : (
+          <div className="bg-white shadow-lg rounded-2xl p-8 max-w-md text-center">
+            <motion.div
+              initial={{ scale: 0 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.5, type: "spring" }}
+              className="flex justify-center"
+            >
+              {success ? (
+                <CheckCircle className="text-green-500 w-20 h-20" />
+              ) : (
+                <OctagonX className="text-red-500 w-20 h-20" />
+              )}
+            </motion.div>
+            <h1 className="text-2xl font-bold mt-4 text-gray-800">
+              {success ? "Payment Successful!" : "Payment Failed!"}
+            </h1>
+            <p className="text-gray-600 mt-2">
+              {success
+                ? `Thank you for your purchase. Your transaction has been completed
+            successfully.`
+                : "Cannot complete your payment! Please try again."}
+            </p>
+
+            <div className="mt-6 space-y-4">
+              <Button asChild>
+                <Link href="/">
+                  <button className="w-full py-2 px-4 rounded-lg transition">
+                    Go to Home
+                  </button>
+                </Link>
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </Suspense>
   );
 }
--- src/app/(root)/verify-email/page.js
+++ src/app/(root)/verify-email/page.js
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button";
 import { AlertCircle, CheckCircle } from "lucide-react";
 import Link from "next/link";
 import { notFound } from "next/navigation";
-import { Suspense } from "react";
 
 async function verifyEmail(token) {
   try {
@@ -37,38 +36,36 @@ const VerifyEmail = async ({ searchParams }) => {
   }
 
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <div className="min-h-screen bg-slate-50 flex items-center justify-center p-6">
-        <div className="bg-white rounded-lg shadow-2xl p-8 max-w-md w-full text-center">
-          <div className="flex justify-center mb-6">
-            {isVerified ? (
-              <CheckCircle className="w-8 h-8 text-green-500" />
-            ) : (
-              <AlertCircle className="w-8 h-8 text-red-400" />
-            )}
-          </div>
-          <h1 className="text-3xl font-bold text-gray-800 mb-4">
-            {isVerified ? "Email Verified!" : "Verification failed!"}
-          </h1>
-          <p className="text-gray-600 mb-6">
-            {isVerified
-              ? `Your email has been successfully verified. You can now access all
+    <div className="min-h-screen bg-slate-50 flex items-center justify-center p-6">
+      <div className="bg-white rounded-lg shadow-2xl p-8 max-w-md w-full text-center">
+        <div className="flex justify-center mb-6">
+          {isVerified ? (
+            <CheckCircle className="w-8 h-8 text-green-500" />
+          ) : (
+            <AlertCircle className="w-8 h-8 text-red-400" />
+          )}
+        </div>
+        <h1 className="text-3xl font-bold text-gray-800 mb-4">
+          {isVerified ? "Email Verified!" : "Verification failed!"}
+        </h1>
+        <p className="text-gray-600 mb-6">
+          {isVerified
+            ? `Your email has been successfully verified. You can now access all
           features of your account.`
-              : "Something went wrong! PLease try again."}
-          </p>
-          <div className="mb-6">
-            <Button asChild>
-              <Link
-                href={isVerified ? "/generate" : "/"} // Replace with your desired redirect path
-                className="inline-block text-white px-6 py-2 rounded-lg transition duration-300"
-              >
-                {isVerified ? "Go to Generate" : "Go to Home"}
-              </Link>
-            </Button>
-          </div>
+            : "Something went wrong! PLease try again."}
+        </p>
+        <div className="mb-6">
+          <Button asChild>
+            <Link
+              href={isVerified ? "/generate" : "/"} // Replace with your desired redirect path
+              className="inline-block text-white px-6 py-2 rounded-lg transition duration-300"
+            >
+              {isVerified ? "Go to Generate" : "Go to Home"}
+            </Link>
+          </Button>
         </div>
       </div>
-    </Suspense>
+    </div>
   );
 };
 
